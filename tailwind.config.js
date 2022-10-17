b0a9f946161627a04432a3a0361e9e85f1fd7689
d855 <dosmokrovic@yahoo.com>
2022-10-17 15:51:00 +0200

    price-component

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      'white': '#ffffff',
+      'cyan': 'hsl(179, 62%, 43%)',
+      'light-cyan': 'hsla(179,62%,43%,0.69)',
+      'bright-yellow': 'hsl(71, 73%, 54%)',
+      'light-gray': 'hsl(204, 43%, 93%)',
+      'grayish-blue': 'hsl(218, 22%, 67%)'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
