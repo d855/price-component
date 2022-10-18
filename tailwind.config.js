6774744155f4fb6fc4635fce2ff388062810ff10
d855 <dosmokrovic@yahoo.com>
2022-10-18 10:18:13 +0200

    readme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html"],
+  content: ["./*.html"],
   theme: {
     colors: {
       transparent: 'transparent',
@@ -8,6 +8,7 @@ module.exports = {
       'cyan': 'hsl(179, 62%, 43%)',
       'light-cyan': 'hsla(179,62%,43%,0.69)',
       'bright-yellow': 'hsl(71, 73%, 54%)',
+      'bright-yellow-hover': 'hsla(71, 73%, 54%, 0.90)',
       'light-gray': 'hsl(204, 43%, 93%)',
       'grayish-blue': 'hsl(218, 22%, 67%)'
     },
